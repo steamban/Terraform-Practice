038847d819c202943863ce5e3a734fdd9a89b146
Arvind Nair <nairarvind2011@gmail.com>
2024-05-04 21:02:14 +0530

    Outputs in Terraform

---
+++ exercise4_outputs/instance.tf
@@ -0,0 +1,41 @@
+resource "aws_key_pair" "dove-key" {
+  key_name   = "dovekey"
+  public_key = file("dovekey.pub")
+}
+
+resource "aws_instance" "dove-inst" {
+  ami                    = var.AMIS[var.REGION]
+  instance_type          = "t2.micro"
+  availability_zone      = "us-east-1a"
+  key_name               = aws_key_pair.dove-key.key_name
+  vpc_security_group_ids = ["sg-04e37cde379a1d3d1"]
+  tags = {
+    Name = "Dove-Instance"
+  }
+
+  provisioner "file" {
+    source      = "web.sh"
+    destination = "/tmp/web.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod u+x /tmp/web.sh",
+      "sudo /tmp/web.sh"
+    ]
+  }
+
+  connection {
+    user        = var.USER
+    private_key = file("dovekey")
+    host        = self.public_ip
+  }
+}
+
+output "PublicIP" {
+  value = aws_instance.dove-inst.public_ip
+}
+
+output "PrivateIP" {
+  value = aws_instance.dove-inst.private_ip
+}
