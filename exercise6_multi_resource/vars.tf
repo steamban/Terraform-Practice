a31b23800eb73a2f381be1d7cf1c10b629fbe2eb
Arvind Nair <nairarvind2011@gmail.com>
2024-05-06 21:08:57 +0530

    Terraform config to create multiple resources

---
+++ exercise6_multi_resource/vars.tf
@@ -0,0 +1,39 @@
+variable "REGION" {
+  default = "us-east-1"
+}
+
+variable "ZONE1" {
+  default = "us-east-1a"
+}
+
+variable "ZONE2" {
+  default = "us-east-1b"
+}
+
+variable "ZONE3" {
+  default = "us-east-1c"
+}
+
+variable "AMIS" {
+  type = map(any)
+  default = {
+    us-east-1 = "ami-0a1179631ec8933d7"
+    us-east-2 = "ami-080e449218d4434fa"
+  }
+}
+
+variable "USER" {
+  default = "ec2-user"
+}
+
+variable "PUB_KEY" {
+  default = "dovekey.pub"
+}
+
+variable "PRIV_KEY" {
+  default = "dovekey"
+}
+
+variable "MYIP" {
+  default = "183.83.67.89/32"
+}
